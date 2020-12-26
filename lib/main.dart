a010a0edd602144e930b68cf94a75439cf898056
Divyanshmandhan-1 <70142854+Divyanshmandhan-1@users.noreply.github.com>
2020-12-26 12:05:00 +0530

    Radio with AI - ASSITANT

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:Radio/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alan-Radio',
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/utilities/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color primaryColor1 = Vx.pink400;
+  static Color primaryColor2 = Vx.blue500;
+}
